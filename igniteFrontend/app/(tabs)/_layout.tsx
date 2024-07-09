b6d0a05c362dc040a272f5f56901d317b11e6f75
ARMummert <135379751+ARMummert@users.noreply.github.com>
2024-07-09 16:55:10 -0700

    layout updates

--- igniteFrontend/app/(tabs)/_layout.tsx
+++ igniteFrontend/app/(tabs)/_layout.tsx
@@ -1,19 +1,20 @@
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-// Remove the duplicate import statement for 'FontAwesome'
-import HomeScreen from './HomeScreen'; // Import the HomeScreen component with the correct file extension
-import { View } from 'react-native';
+import HomeScreen from './HomeScreen'; 
+import { View, StyleSheet } from 'react-native';
 import { Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button} from 'react-native-elements';
 import SettingsScreen  from './SettingsScreen'; 
 import AccountProfileScreen from './AccountProfileScreen';
-import LoginScreen from './LoginScreen';
+import LoginScreen from './loginScreen';
 import * as React from 'react';
 import { useFonts } from 'expo-font';
-import { FontAwesome } from '@expo/vector-icons'; // Add this import statement
+import { FontAwesome } from '@expo/vector-icons'; 
 import { useColorScheme } from '@/components/useColorScheme';
 import PrivacyPolicy  from './PrivacyPolicy';
-
+import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity } from 'react-native';
+import { GestureResponderEvent } from 'react-native';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -26,21 +27,58 @@ function TabBarIcon(props: {
 const Drawer = createDrawerNavigator();
 
 function DrawerContent(props: any) {
-  // Add your dropdown menu items and navigation logic here
+  function onPress(event: GestureResponderEvent): void {
+    throw new Error('Function not implemented.');
+  }
+   // Load custom fonts
+   const [fontsLoaded] = useFonts({
+    'Alkatra': require('../../assets/fonts/Alkatra-VariableFont_wght.ttf'), 
+    'AguafinaScript-Regular': require('../../assets/fonts/AguafinaScript-Regular.ttf'),
+  });
+   
+  if (!fontsLoaded) {
+   <Text>Loading...</Text>;
+  }
   return (
-    <View style={{ flex: 1, padding: 20 }}>
-      <Text>Dropdown Menu</Text>
-      {/* Add your menu items here */}
-      <Button title="Home" onPress={() => props.navigation.navigate('Home')} />
-      <Button title="Login" onPress={() => props.navigation.navigate('LoginScreen')} />
-      <Button title='Account Profile' onPress={() => props.navigation.navigate('AccountProfileScreen')} />
-      <Button title="Appearance/Theme" onPress={() => props.navigation.navigate('AppearanceTheme')} />
-      <Button title="Retake Fitness Survey" onPress={() => props.navigation.navigate('RetakeFitnessSurvey')} />
-      <Button title="Login" onPress={() => props.navigation.navigate('LoginScreen')} />
-      <Button title="Privacy Policy" onPress={() => props.navigation.navigate('PrivacyPolicy')} />
-      <Button title="Security/Privacy" onPress={() => props.navigation.navigate('SecurityPrivacy')} />
-      <Button title="Settings" onPress={() => props.navigation.navigate('Settings')} /> 
-    </View>
+   
+    <View style={styles.container}>
+       
+    <View style={styles.navigationButtonContainer}>
+     
+      <Text style={styles.headerTitle}>Ignite</Text>
+
+      <LinearGradient colors={['#F83600', '#FE8C00']} style={styles.gradient}>
+      <TouchableOpacity style={styles.navigationButton} onPress={() => props.navigation.navigate('Home')}>
+        <Text style={styles.buttonText}>Home</Text>
+      </TouchableOpacity>
+      </LinearGradient>
+      <LinearGradient colors={['#F83600', '#FE8C00']} style={styles.gradient}>
+      <TouchableOpacity style={styles.navigationButton} onPress={() => props.navigation.navigate('LoginScreen')}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      </LinearGradient>
+      <LinearGradient colors={['#F83600', '#FE8C00']} style={styles.gradient}>
+      <TouchableOpacity style={styles.navigationButton} onPress={() => props.navigation.navigate('AccountProfileScreen')}>
+        <Text style={styles.buttonText}>Account Profile</Text>
+      </TouchableOpacity>
+      </LinearGradient>
+      <LinearGradient colors={['#F83600', '#FE8C00']} style={styles.gradient}>
+      <TouchableOpacity style={styles.navigationButton} onPress={() => props.navigation.navigate('AppearanceTheme')}>
+        <Text style={styles.buttonText}>Appearance / Theme </Text>
+      </TouchableOpacity>
+      </LinearGradient>
+      <LinearGradient colors={['#F83600', '#FE8C00']} style={styles.gradient}>
+      <TouchableOpacity style={styles.navigationButton} onPress={() => props.navigation.navigate('RetakeFitnessSurvey')}>
+        <Text style={styles.buttonText}>Retake Fitness Survey</Text>
+      </TouchableOpacity>
+      </LinearGradient>
+      <LinearGradient colors={['#F83600', '#FE8C00']} style={styles.gradient}>
+      <TouchableOpacity style={styles.navigationButton} onPress={() => props.navigation.navigate('LoginScreen')}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      </LinearGradient>
+  </View>
+  </View>
   );
 }
 
@@ -52,30 +90,27 @@ export default function TabLayout() {
       drawerContent={(props) => <DrawerContent {...props} />}
       screenOptions={{
         headerShown: useClientOnlyValue(false, true),
-      }}
+      }}  
     >
       <Drawer.Screen
         name="Home"
-        component={HomeScreen} // Replace with your actual home screen component
+        component={HomeScreen} 
         options={{
           title: 'Dashboard',
-          // Remove tabBarIcon as it's not used in Drawer
-          // drawerIcon can be used to set the drawer menu icon
           drawerIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
       <Drawer.Screen
         name="Settings"
-        component={SettingsScreen} // Replace with your actual settings screen component
+        component={SettingsScreen} 
         options={{
           title: 'Settings',
-          // Remove tabBarIcon as it's not used in Drawer
           drawerIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
       <Drawer.Screen
         name="AccountProfileScreen"
-        component={AccountProfileScreen} // Replace with your actual account profile screen component
+        component={AccountProfileScreen} 
         options={{
           title: 'Account Profile',
           drawerIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
@@ -83,7 +118,7 @@ export default function TabLayout() {
         />
       <Drawer.Screen
         name="LoginScreen"
-        component={LoginScreen} // Replace with your actual login screen component
+        component={LoginScreen} 
         options={{
           title: 'Login',
           drawerIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
@@ -91,7 +126,7 @@ export default function TabLayout() {
         />
       <Drawer.Screen
         name="PrivacyPolicy"
-        component={PrivacyPolicy} // Replace with your actual privacy policy screen component
+        component={PrivacyPolicy} 
         options={{
           title: 'Privacy Policy',
           drawerIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
@@ -100,4 +135,69 @@ export default function TabLayout() {
     </Drawer.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  navigationButtonContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    margin: 25,
+    padding: 20,
+    backgroundColor: 'black',
+    width: '75%',
+  },
+  navigationButton: {
+    marginBottom: 10,
+    marginTop: 10,
+    
+  },
+  headerTitle: {
+    flex: 1,
+    fontSize: 60,
+    justifyContent: 'center',
+    marginBottom: 50,
+    alignSelf: 'center',
+    color: '#F83600', 
+    fontFamily: 'AguafinaScript-Regular',
+    
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  icon: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: 'black',
+    backgroundColor: 'white',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5, 
+  },
+  gradient: {
+    width: '100%',
+    borderRadius: 5,
+    marginBottom: 15,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '400',
+    fontFamily: 'Alkatra-VariableFront_wght',
+  },
+  text: {
+    color: 'black',
+    fontSize: 16,
+    fontFamily: 'Arial',
+  },
+});
 
