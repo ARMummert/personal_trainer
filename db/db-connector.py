b4a2511c9a4383d0368c3ec3a628c77156c7c3fa
Alexander Dembo <demboa@oregonstate.edu>
2024-07-22 12:23:13 -0700

    Current Iteration of DML, Updated DDL and db-connector.py for OSU MariaDB MySQL server

--- db/db-connector.py
+++ db/db-connector.py
@@ -9,14 +9,15 @@ app = Flask(__name__)
 
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_IP_ADDRESS = os.getenv('DB_IP_ADDRESS')
+DB_HOST = os.getenv('DB_IP_ADDRESS')
 DB_NAME = os.getenv('DB_NAME')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:'
-f'{DB_PASSWORD}@{DB_IP_ADDRESS}/{DB_NAME}'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+mysqldb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
-    app.run
+    app.run(debug=True)
